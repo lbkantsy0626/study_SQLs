d9e354b9bb9853168a5e08bd751023bf99edc996
tjrwls234 <tjrwls234@gmail.com>
2022-10-18 15:12:53 +0900

    실습 추가 커밋

--- seokjin/w3schools/Categories_Products_Count.sql
+++ seokjin/w3schools/Categories_Products_Count.sql
@@ -1,4 +1,7 @@
--- Categories 별로 구성된 제품 갯수 표시하세요.
-SELECT COUNT(ProductID), CategoryID
+-- Categories 별로
+-- 구성된 제품 갯수, 가격 평균, 최고값, 최저값 표시하세요.
+SELECT CategoryID, COUNT(ProductID), AVG(Price), MAX(Price), MIN(Price)
 FROM Products
 GROUP BY CategoryID;
+
+-- https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_select_groupby
