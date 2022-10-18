e6b992b6fe91d79da0ea38ab8c2bddd84c8b6233
tjrwls234 <tjrwls234@gmail.com>
2022-10-18 13:41:19 +0900

    주석 수정

--- seokjin/w3schools/CategoryName_Products.sql
+++ seokjin/w3schools/CategoryName_Products.sql
@@ -1,3 +1,5 @@
+-- CategoryName 가 Dairy Products, Seafood이고
+-- Price 10.00이상 50.00이하인 합계와 평균
 SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID IN (SELECT CategoryID
--- seokjin/w3schools/CategoryName_Products_Order.sql
+++ seokjin/w3schools/CategoryName_Products_Order.sql
@@ -1,3 +1,6 @@
+--CategoryName 가 Dairy Products, Seafood이고
+--Price 10.00이상 50.00이하인 주문 OrderID 내역과 총 갯수
+
 -- OrderID 내역
 SELECT DISTINCT OrderID
 FROM OrderDetails
