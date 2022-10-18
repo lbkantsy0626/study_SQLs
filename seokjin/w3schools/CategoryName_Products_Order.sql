8f7c8a69c1ee4b092a65591d77a16cc7b3a78747
tjrwls234 <tjrwls234@gmail.com>
2022-10-18 13:38:35 +0900

    실습 수정

--- seokjin/w3schools/CategoryName_Products_Order.sql
+++ seokjin/w3schools/CategoryName_Products_Order.sql
@@ -1,4 +1,15 @@
-SELECT DISTINCT OrderID, COUNT(*)
+-- OrderID 내역
+SELECT DISTINCT OrderID
+FROM OrderDetails
+WHERE ProductID IN (SELECT ProductID
+					FROM Products
+					WHERE CategoryID IN (SELECT CategoryID
+											FROM Categories
+											WHERE CategoryName IN ('Dairy Products', 'Seafood')
+					AND Price BETWEEN 10.00 AND 50.00));
+
+-- OrderID 총 갯수
+SELECT COUNT(*)
 FROM OrderDetails
 WHERE ProductID IN (SELECT ProductID
 					FROM Products
