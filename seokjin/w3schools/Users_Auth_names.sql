a881c257f902b12cc5e40f57db0064c663dc6f19
tjrwls234 <tjrwls234@gmail.com>
2022-10-20 14:58:39 +0900

    Users_Auth_names 커밋

--- seokjin/w3schools/Users_Auth_names.sql
+++ seokjin/w3schools/Users_Auth_names.sql
@@ -1,5 +1,6 @@
 -- INNER JOIN
--- + 4-1: 이름, 이메일, 권한이름 표시 ?
+-- + 4-1: 이름, 이메일, 권한이름 표시 
+-- 조건 이름이 'Teddy', 'Allen' ?
 
 SELECT users.NAME, users.EMAIL, auth_names.AUTH_NAME
 FROM (users INNER JOIN auths
