867e192f6e3894bc6566781ccd008dd77e6d0266
tjrwls234 <tjrwls234@gmail.com>
2022-10-20 14:55:56 +0900

    Users_Auth_names 커밋

---
+++ seokjin/w3schools/Users_Auth_names.sql
@@ -0,0 +1,10 @@
+-- INNER JOIN
+-- + 4-1: 이름, 이메일, 권한이름 표시 ?
+
+SELECT users.NAME, users.EMAIL, auth_names.AUTH_NAME
+FROM (users INNER JOIN auths
+ON users.UNIQUE_ID = auths.UNIQUE_ID_USERS
+AND users.NAME IN ('Teddy', 'Allen')) INNER JOIN auth_names
+ ON auths.UNIQUE_ID_AUTH_NAMES = auth_names.UNIQUE_ID
+;
+
